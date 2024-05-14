3cc023e578743f7ef2ec63fa5ba0e1034ca03609
Steven Bondoux <bondoux.s@gmail.com>
2024-05-14 19:47:03 +0200

    Repare fonctionality of gallery

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,42 +1,30 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $('.gallery').mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3
+    },
+    lightBox: true,
+    lightboxId: 'myAwesomeLightbox',
+    showTags: true,
+    tagsPosition: 'top'
+  });
 });
 
-const gallerySrc = [
-    "./assets/images/gallery/concerts/aaron-paul-concert.jpg",
-    "./assets/images/gallery/entreprise/ali-morshedlou.jpg",
-    "./assets/images/gallery/entreprise/jason-goodman.jpg",
-    "./assets/images/gallery/mariage/hannah-busing.jpg",
-    "./assets/images/gallery/portraits/ade-tunji.jpg",
-    "./assets/images/gallery/mariage/jakob-owens.jpg",
-    "./assets/images/gallery/portraits/nino-van-prattenburg.jpg",
-    "./assets/images/gallery/concerts/austin-neill-concert.jpg",
-    "./assets/images/gallery/entreprise/mateus-campos-felipe.jpg"
+let galleryImages = [
+  "./assets/images/gallery/concerts/aaron-paul-concert.jpg",
+  "./assets/images/gallery/entreprise/ali-morshedlou.jpg",
+  "./assets/images/gallery/entreprise/jason-goodman.jpg",
+  "./assets/images/gallery/mariage/hannah-busing.jpg",
+  "./assets/images/gallery/portraits/ade-tunji.jpg",
+  "./assets/images/gallery/mariage/jakob-owens.jpg",
+  "./assets/images/gallery/portraits/nino-van-prattenburg.jpg",
+  "./assets/images/gallery/concerts/austin-neill-concert.jpg",
+  "./assets/images/gallery/entreprise/mateus-campos-felipe.jpg"
 ];
-/*cible ma modale*/ 
-/*const openModal = document.querySelector('.modal-open');
-
-const previousImg = document.querySelector('.mg-prev');
-const nextImg = document.querySelector('.mg-next');
-
-openModal.addEventListener('shown.bs.modal', function () {
-    previousImg.addEventListener('click', () => {
-        const imgBox = document.querySelector('.lightboxImage')
-        console.log(imgBox)
-    })
-})*/
 
 // Selectionne le noeud dont les mutations seront observées
 let targetNode = document.getElementById("body");
@@ -46,10 +34,80 @@ let config = { attributes: true, childList: false };
 
 // Fonction callback à éxécuter quand une mutation est observée
 let callback = function (mutationsList) {
-  for (let mutation of mutationsList){
+  for (let mutation of mutationsList) {
     if (mutation.type == "attributes" && mutation.attributeName == "class") {
-      if (targetNode.classList.contains('modal-open')){
-        console.log(targetNode.classList.value);
+      if (targetNode.classList.contains('modal-open')) {
+        const iconeNext = document.querySelector('.mg-next')
+        const iconePrev = document.querySelector('.mg-prev')
+        let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src
+        let urlRelative = absoluVersRelatif(imageSourceModal);
+
+        const actualIndice = galleryImages.indexOf(urlRelative)
+
+        if(actualIndice === 0)
+          document.querySelector('.mg-prev').classList.add('hide')
+        else if (actualIndice + 1 === galleryImages.length)
+          document.querySelector('.mg-next').classList.add('hide')
+        /*let galleryUrlLenght = galleryImages.lenght;
+
+        let currentIndex = 0;
+        for (let i = 1; i < galleryUrlLenght; i++) {
+          let indexTosearch = i - 1;
+          if (urlRelative === galleryUrlLenght.indexTosearch) {
+            currentIndex = indexTosearch;
+            // affichage des fleches
+            if (indexTosearch === 0) {
+              // je doit cacher ma fleche prev
+            } else if (indexTosearch === 8) {
+              // je doit cacher ma fleche next
+            } else {
+              // je doit faire appraitre mes deux fleches
+            }
+          }
+
+          // source
+          // j'ecoute le click prev
+          // je change la source en utilisant l'index prev
+          //new src = tableUrl.currentIndex - 1
+
+          // j'ecoute le click next
+          // je change la source en utilisant l'index next
+          //new src = tableUrl.currentIndex + 1
+        }*/
+
+
+
+
+
+
+
+        iconeNext.addEventListener('click', () => {
+          document.querySelector('.mg-prev').classList.remove('hide')
+          let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src
+          let urlRelative = absoluVersRelatif(imageSourceModal);
+          const actualIndice = galleryImages.indexOf(urlRelative)
+          const nextImage = galleryImages[actualIndice + 1]
+          if (nextImage !== undefined) {
+            document.querySelector('.lightboxImage.img-fluid').src = nextImage
+            if (actualIndice + 2 === galleryImages.length)
+              document.querySelector('.mg-next').classList.add('hide')
+            
+          }
+
+        })
+        iconePrev.addEventListener('click', () => {
+          document.querySelector('.mg-next').classList.remove('hide')
+          let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src
+          let urlRelative = absoluVersRelatif(imageSourceModal);
+          const actualIndice = galleryImages.indexOf(urlRelative)
+          const nextImage = galleryImages[actualIndice - 1]
+          if (nextImage !== undefined) {
+            document.querySelector('.lightboxImage.img-fluid').src = nextImage
+            if (actualIndice - 1 === 0 )
+              document.querySelector('.mg-prev').classList.add('hide')
+          }
+        })
+
       }
     }
   }
@@ -61,7 +119,36 @@ let observer = new MutationObserver(callback);
 // Commence à observer le noeud cible pour les mutations précédemment configurées
 observer.observe(targetNode, config);
 
-// L'observation peut être arrêtée par la suite
+
+
+function absoluVersRelatif(urlAbsolue) {
+  // Obtenir l'URL absolue de la page actuelle
+  let urlPageActuelle = window.location.href;
+
+  // Diviser les URLs en parties
+  let partiesUrlAbsolue = urlAbsolue.split('/');
+  let partiesUrlPageActuelle = urlPageActuelle.split('/');
+
+  // Trouver l'indice de la première partie différente
+  let indiceDiff = 0;
+  while (partiesUrlAbsolue[indiceDiff] === partiesUrlPageActuelle[indiceDiff]) {
+    indiceDiff++;
+  }
+
+  // Construire le chemin relatif
+  let cheminRelatif = '';
+  for (let i = indiceDiff; i < partiesUrlPageActuelle.length - 1; i++) {
+    cheminRelatif += '../';
+  }
+
+  for (let j = indiceDiff; j < partiesUrlAbsolue.length; j++) {
+    cheminRelatif += partiesUrlAbsolue[j];
+    if (j < partiesUrlAbsolue.length - 1) {
+      cheminRelatif += '/';
+    }
+  }
+  return './' + cheminRelatif;
+}
 
 
 
