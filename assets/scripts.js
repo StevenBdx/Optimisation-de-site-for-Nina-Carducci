ef72cbb098e8f43cc9bde1950b6cbac6816f5439
Steven Bondoux <bondoux.s@gmail.com>
2024-05-24 14:35:04 +0200

    Refacto global clean code

--- assets/scripts.js
+++ assets/scripts.js
@@ -26,110 +26,92 @@ let galleryImages = [
   "./assets/images/gallery/entreprise/mateus-campos-felipe.jpg"
 ];
 
-// Selectionne le noeud dont les mutations seront observées
+// Select the node whose mutations will be observed
 let targetNode = document.getElementById("body");
 
-// Options de l'observateur (quelles sont les mutations à observer)
+// Observer options (what mutations to observe)
 let config = { attributes: true, childList: false };
 
-// Fonction callback à éxécuter quand une mutation est observée
+// Callback function to execute when a mutation is observed
 let callback = function (mutationsList) {
   for (let mutation of mutationsList) {
     if (mutation.type == "attributes" && mutation.attributeName == "class") {
       if (targetNode.classList.contains('modal-open')) {
-        const iconeNext = document.querySelector('.mg-next')
-        const iconePrev = document.querySelector('.mg-prev')
+        const iconeNext = document.querySelector('.mg-next');
+        const iconePrev = document.querySelector('.mg-prev');
 
-        let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src
+        let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src;
         let urlRelative = absoluVersRelatif(imageSourceModal);
 
-        const actualIndice = galleryImages.indexOf(urlRelative)
+        const actualIndice = galleryImages.indexOf(urlRelative);
 
         if (actualIndice === 0) {
-          document.querySelector('.mg-prev').classList.add('hide')
-          document.querySelector('.mg-next').classList.remove('hide')
+          document.querySelector('.mg-prev').classList.add('hide');
+          document.querySelector('.mg-next').classList.remove('hide');
         } else if (actualIndice + 1 === galleryImages.length) {
-          document.querySelector('.mg-next').classList.add('hide')
-          document.querySelector('.mg-prev').classList.remove('hide')
+          document.querySelector('.mg-next').classList.add('hide');
+          document.querySelector('.mg-prev').classList.remove('hide');
         } else {
-          document.querySelector('.mg-next').classList.remove('hide')
-          document.querySelector('.mg-prev').classList.remove('hide')
+          document.querySelector('.mg-next').classList.remove('hide');
+          document.querySelector('.mg-prev').classList.remove('hide');
         }
-
-
-       
       }
     }
   }
 };
 
-/**
- * A SAUVEGARDER
- */
 document.addEventListener('click', function (event) {
 
   if (event.target.classList.contains('mg-prev') || event.target.classList.contains('mg-next')) {
-    const iconePrev = document.querySelector('.mg-prev')
-    const iconeNext = document.querySelector('.mg-next')
-    let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src
+    const iconePrev = document.querySelector('.mg-prev');
+    const iconeNext = document.querySelector('.mg-next');
+    let imageSourceModal = document.querySelector('.lightboxImage.img-fluid').src;
     let urlRelative = absoluVersRelatif(imageSourceModal);
-    const actualIndice = galleryImages.indexOf(urlRelative)
+    const actualIndice = galleryImages.indexOf(urlRelative);
     let image = '';
 
     if (event.target.classList.contains('mg-next')) {
       image = galleryImages[actualIndice + 1]
-
-      iconePrev.classList.remove('hide')
+      iconePrev.classList.remove('hide');
       if (actualIndice + 1 === galleryImages.length - 1) {
-        iconeNext.classList.add('hide')
+        iconeNext.classList.add('hide');
       }
     }
-
     if (event.target.classList.contains('mg-prev')) {
       image = galleryImages[actualIndice - 1]
 
-      iconeNext.classList.remove('hide')
+      iconeNext.classList.remove('hide');
 
       if (actualIndice - 1 <= 0) {
-        iconePrev.classList.add('hide')
+        iconePrev.classList.add('hide');
       }
     }
 
     document.querySelector('.lightboxImage.img-fluid').src = image
-
   }
 })
-/**
- * FIN de A SAUVEGARDER
- */
 
-
-
-
-
-// Créé une instance de l'observateur lié à la fonction de callback
+// Creates an instance of the observer linked to the callback function
 let observer = new MutationObserver(callback);
 
-// Commence à observer le noeud cible pour les mutations précédemment configurées
+// Starts observing the target node for previously configured mutations
 observer.observe(targetNode, config);
 
-
-
 function absoluVersRelatif(urlAbsolue) {
-  // Obtenir l'URL absolue de la page actuelle
+  // Get absolute URL of current page
   let urlPageActuelle = window.location.href;
 
-  // Diviser les URLs en parties
+  // Divide Urls into parts
   let partiesUrlAbsolue = urlAbsolue.split('/');
   let partiesUrlPageActuelle = urlPageActuelle.split('/');
 
-  // Trouver l'indice de la première partie différente
+  // Find the clue of the first different part
   let indiceDiff = 0;
   while (partiesUrlAbsolue[indiceDiff] === partiesUrlPageActuelle[indiceDiff]) {
     indiceDiff++;
   }
 
-  // Construire le chemin relatif
+  // Construct the relative path
   let cheminRelatif = '';
   for (let i = indiceDiff; i < partiesUrlPageActuelle.length - 1; i++) {
     cheminRelatif += '../';
